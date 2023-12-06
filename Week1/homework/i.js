5ae25cfbd01f9e6b6a2fe861682a19d506f40661
Enes <snggenes@gmail.com>
2023-12-06 09:29:34 +0100

    correction

--- Week1/homework/i.js
+++ Week1/homework/i.js
@@ -7,106 +7,78 @@ var connection = mysql.createConnection({
 });
 connection.connect();
 
-connection.query("create database if not exists meetup", (error, result) => {
-  if (error) throw error;
-  console.log(result);
-});
-
-connection.query("use meetup", (error, result) => {
-  if (error) throw error;
-  console.log(result);
-});
-
-connection.query(
-  `
-    create table Invitee(
-        invitee_no int auto_increment primary key,
-        invitee_name text,
-        invited_by text
-    )
-`,
-  (error, result) => {
-    if (error) throw error;
+function executeQuery(query) {
+  connection.query(query, (error, result) => {
+    if (error) {
+      console.error(`Error: ${query}`);
+      throw error;
+    }
     console.log(result);
-  }
-);
-connection.query(
-  `
-      create table Room(
-          room_no int auto_increment primary key,
-          room_name text,
-          floor_number int
-      )
-  `,
-  (error, result) => {
-    if (error) throw error;
-    console.log(result);
-  }
-);
+  });
+}
 
-connection.query(
-  `
-        create table Meeting(
-            meeting_no int auto_increment primary key,
-            meeting_title text,
-            starting_time text,
-            ending_time text,
-            room_no int,
-            foreign key (room_no) references Room(room_no)
-        )
-    `,
-  (error, result) => {
-    if (error) throw error;
-    console.log(result);
-  }
-);
+const query1 = "create database if not exists meetup";
+const query2 = "use meetup";
+const query3 = `
+create table if not exists Invitee(
+    invitee_no int auto_increment primary key,
+    invitee_name text,
+    invited_by int,
+    foreign key (invited_by) references Invitee(invitee_no)
+)
+`;
+const query4 = `
+create table if not exists Room(
+    room_no int auto_increment primary key,
+    room_name text,
+    floor_number int
+)
+`;
+const query5 = `
+create table if not exists Meeting(
+    meeting_no int auto_increment primary key,
+    meeting_title text,
+    starting_time text,
+    ending_time text,
+    room_no int,
+    foreign key (room_no) references Room(room_no)
+)
+`;
+const query6 = `
+INSERT INTO Invitee (invitee_name, invited_by)
+VALUES
+('Sven', NULL), -- Assuming Sven is not invited by anyone
+('Eva', 1),     -- Assuming Eva is invited by invitee_no 1
+('Lars', 2),     -- Assuming Lars is invited by invitee_no 2
+('Anna', 3),     -- Assuming Anna is invited by invitee_no 3
+('Johan', 4)     -- Assuming Johan is invited by invitee_no 4
+`;
+const query7 = `
+INSERT INTO Room (room_name, floor_number)
+VALUES
+('Room A', 1),
+('Room B', 2),
+('Room C', 1),
+('Room D', 1),
+('Room E', 2)
+`;
+const query8 = `
+INSERT INTO Meeting (meeting_title, starting_time, ending_time, room_no)
+VALUES
+('Project A', '19-11-1997 10:00:00', '19-11-1997 11:00:00', 1),
+('Project B', '20-11-1997 10:00:00', '20-11-1997 11:00:00', 2),
+('Project C', '21-11-1997 10:00:00', '21-11-1997 11:00:00', 3),
+('Project D', '22-11-1997 10:00:00', '22-11-1997 11:00:00', 4),
+('Project E', '23-11-1997 10:00:00', '23-11-1997 11:00:00', 5)
+`;
 
-connection.query(
-  `
-      INSERT INTO Invitee (invitee_name, invited_by)
-      VALUES
-      ('Sven', 'Sofia'),
-      ('Eva', 'Erik'),
-      ('Lars', 'Emma'),
-      ('Anna', 'Gustav'),
-      ('Johan', 'Isabella')
-    `,
-  (error, result) => {
-    if (error) throw error;
-    console.log(result);
-  }
-);
-
-connection.query(
-  `
-      INSERT INTO Room (room_name, floor_number)
-      VALUES
-      ('Room A', 1),
-      ('Room B', 2),
-      ('Room C', 1),
-      ('Room D', 1),
-      ('Room E', 2)
-    `,
-  (error, result) => {
-    if (error) throw error;
-    console.log(result);
-  }
-);
-
-connection.query(
-  `
-      INSERT INTO Meeting (meeting_title, starting_time, ending_time, room_no)
-      VALUES
-      ('Project A', '19-11-1997 10:00:00', '19-11-1997 11:00:00', 1),
-      ('Project B', '20-11-1997 10:00:00', '20-11-1997 11:00:00', 2),
-      ('Project C', '21-11-1997 10:00:00', '21-11-1997 11:00:00', 3),
-      ('Project D', '22-11-1997 10:00:00', '22-11-1997 11:00:00', 4),
-      ('Project E', '23-11-1997 10:00:00', '23-11-1997 11:00:00', 5)
-    `,
-  (error, result) => {
-    if (error) throw error;
-    console.log(result);
-  }
-);
+executeQuery(query1);
+executeQuery(query2);
+executeQuery(query3);
+executeQuery(query4);
+executeQuery(query5);
+executeQuery(query6);
+executeQuery(query7);
+executeQuery(query8);
 
 connection.end();
--- Week1/homework/world.js
+++ Week1/homework/world.js
@@ -8,72 +8,40 @@ var connection = mysql.createConnection({
 });
 connection.connect();
 
-connection.query(
-  "select Name from country where Population > 8000000;",
-  (error, result) => {
-    if (error) throw error;
+function executeQuery(query) {
+  connection.query(query, (error, result) => {
+    if (error) {
+      console.error(`Error: ${query}`);
+      throw error;
+    }
     console.log(result);
-  }
-);
-connection.query(
-  "select Name from country where name like '%land%'",
-  (error, result) => {
-    if (error) throw error;
-    console.log(result);
-  }
-);
-connection.query(
-  "select Name from city where population between 500000 and 1000000",
-  (error, result) => {
-    if (error) throw error;
-    console.log(result);
-  }
-);
-connection.query(
-  "select Name from country where Continent = 'Europe'",
-  (error, result) => {
-    if (error) throw error;
-    console.log(result);
-  }
-);
-connection.query(
-  "select Name, SurfaceArea from country order by Surfacearea desc",
-  (error, result) => {
-    if (error) throw error;
-    console.log(result);
-  }
-);
-connection.query(
-  "select name from city where CountryCode = 'NLD'",
-  (error, result) => {
-    if (error) throw error;
-    console.log(result);
-  }
-);
-connection.query(
-  "select Population from city where Name = 'Rotterdam'",
-  (error, result) => {
-    if (error) throw error;
-    console.log(result);
-  }
-);
-connection.query(
-  "select Name, SurfaceArea from country order by SurfaceArea desc limit 10",
-  (error, result) => {
-    if (error) throw error;
-    console.log(result);
-  }
-);
-connection.query(
-  "select Name, Population from city order by Population desc limit 10",
-  (error, result) => {
-    if (error) throw error;
-    console.log(result);
-  }
-);
-connection.query("select sum(Population) from country", (error, result) => {
-  if (error) throw error;
-  console.log(result);
-});
+  });
+}
+
+const query1 = "select Name from country where Population > 8000000";
+const query2 = "select Name from country where name like '%land%'";
+const query3 =
+  "select Name from city where population between 500000 and 1000000";
+const query4 = "select Name from country where Continent = 'Europe'";
+const query5 =
+  "select Name, SurfaceArea from country order by Surfacearea desc";
+const query6 = "select name from city where CountryCode = 'NLD'";
+const query7 = "select Population from city where Name = 'Rotterdam'";
+const query8 =
+  "select Name, SurfaceArea from country order by SurfaceArea desc limit 10";
+const query9 =
+  "select Name, Population from city order by Population desc limit 10";
+const query10 = "select sum(Population) from country";
+
+executeQuery(query1);
+executeQuery(query2);
+executeQuery(query3);
+executeQuery(query4);
+executeQuery(query5);
+executeQuery(query6);
+executeQuery(query7);
+executeQuery(query8);
+executeQuery(query9);
+executeQuery(query10);
 
 connection.end();
